1e4cbfbf99d677d4a713a7b0083f5b6e3baac338
Ashish <info.ashdude01@gmail.com>
2024-09-30 20:05:24 +0530

    dp updated

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ProfileImg from "../public/img/profileImg.jpg";
+// import ProfileImg from "../public/img/profileImg.jpg";
 
 const Banner = () => {
   return (
@@ -8,14 +8,16 @@ const Banner = () => {
       <div className="w-44 h-44 rounded-full bg-black">
         <Image
           className="w-full h-full rounded-full border-[1px] border-indigo-700 p-2 object-cover"
-          src={ProfileImg}
+          src='https://i.imgur.com/P5uM15D.jpeg'
+          width={300}
+          height={300}
           alt="ProfileImg"
         />
       </div>
       {/* Description */}
       <div className="w-3/4 flex flex-col items-center md:items-start    gap-2">
-        <h1 className="text-5xl font-bold text-white">Bittu Sharma</h1>
-        <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
+        <h1 className="text-3xl md:text-5xl font-bold whitespace-nowrap text-white">Bittu Sharma</h1>
+        <h3 className="text-xl md:text-2xl whitespace-nowrap font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
             DevOps & Cloud Engineer
         </h3>
         <p className="text-base tracking-wide text-center md:text-left">
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['i.imgur.com'], // Replace 'example.com' with the domain you want to allow
+      },
+}
 
 module.exports = nextConfig
